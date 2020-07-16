2885cc8d8a2a4f937afde19134389a38982829c6
er1c <ericzhao96@hotmail.com>
2020-07-17 00:04:02 +0800

    [add] simple redirect

--- digger.go
+++ digger.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/er1c-zh/go-now/log"
+	"io"
 	"net"
 )
 
@@ -47,5 +49,37 @@ func (d *Digger) Handler(conn net.Conn) {
 		log.Error("Read fail: %s", err.Error())
 		return
 	}
-	log.Info("%s", string(buf[:n]))
+
+	// request line
+	// method SPACE request-target SP HTTP-version CRLF
+	var method, target, version string
+	requestLine := string(buf[:bytes.IndexByte(buf, '\n')])
+	requestLineMatchedCnt, err := fmt.Sscanf(requestLine,
+		"%s %s %s", &method, &target, &version)
+	if err != nil {
+		log.Error("parse request line(%s) fail: %s", requestLine, err.Error())
+		return
+	}
+	if requestLineMatchedCnt != 3 {
+		log.Error("invalid request line(%s) : %s %s %s", requestLine, method, target, version)
+		return
+	}
+
+	log.Info("[%s][%s]%s", method, version, target)
+
+
+	conn2Server, err := net.Dial("tcp", target)
+	if err != nil {
+		log.Error("conn to %s fail: %s", target, err.Error())
+		return
+	}
+
+	if method == "CONNECT" {
+		_, _ = conn.Write([]byte(version + " 200 \n\n"))
+	} else {
+		_, _ = conn2Server.Write(buf[:n])
+	}
+
+	go io.Copy(conn, conn2Server)
+	go io.Copy(conn2Server, conn)
 }
